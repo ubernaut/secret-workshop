d06ae969dd073f22e282829ed6ec51d0a8f3e1f8
ubernaut <collin.schroeder@gmail.com>
2022-09-19 12:20:57 -0800

    works

--- sw-client/src/App.js
+++ sw-client/src/App.js
@@ -1,17 +1,26 @@
 import logo from './gear.svg';
 import './App.css';
+import init from './HelloThree.js'
+import React from 'react'
 
 function App() {
+  const container = React.useRef()
+  const canvas = React.useRef()
+  React.useEffect( ()=> {
+    if (container.current) { init({canvas:canvas.current, container:container.current }) }})
+
   return (
     <div className="App">
-      <header className="App-header">
-      <img id="logo1" src={logo} className="App-logo" alt="logo" />
+      <header className="App-header">      
+        <div id="container"  ref={container} style={{width:'90vw', height:'70vh'}}>
+        <canvas id="canvas"  ref={canvas} style={{width:'90vw', height:'70vh'}}></canvas>
+        </div>
         <p>
           Secret Workshop
         </p>
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ sw-client/src/HelloThree.js
@@ -0,0 +1,66 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { onResize } from './utils.js'
+
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import * as logo from './swcube2textured.gltf'
+
+export const init = ({ canvas, container }) => {
+  const scene = new THREE.Scene()
+  scene.background = new THREE.Color(0x000000)
+
+
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    canvas.clientWidth / canvas.clientHeight,
+    0.1,
+    2000
+  )
+  camera.updateProjectionMatrix()
+  camera.position.set(2, 1, 2)
+
+  const controls = new OrbitControls(camera, canvas)
+  controls.update()
+
+  let renderer = new THREE.WebGLRenderer({ canvas, antialias: true })
+  renderer.setSize(canvas.clientWidth, canvas.clientHeight)
+
+  const handleResize = (event) => {
+    event.preventDefault()
+    onResize({ canvas, camera, renderer })
+  }
+  window.addEventListener('resize', handleResize, false)
+  onResize({ canvas, camera, renderer })
+
+  const geometry = new THREE.BoxGeometry(1, 1, 1)
+  const material = new THREE.MeshLambertMaterial({ color: 0xaaff00 })
+  const cube = new THREE.Mesh(geometry, material)
+  scene.add(cube)
+
+  const light = new THREE.PointLight(0xffffff, 1, 100)
+  light.position.set(0, 3, 5)
+  scene.add(light)
+
+  const clock = new THREE.Clock()
+  const animate = () => {
+    if (!renderer) {
+      return
+    }
+    //stats.begin()
+    requestAnimationFrame(animate)
+    renderer.render(scene, camera)
+    cube.rotateY(clock.getDelta() * 0.5)
+    //stats.end()
+  }
+  animate()
+
+  return () => {
+    renderer.dispose()
+    //stats.scene = null
+    //container.removeChild(stats.dom)
+    renderer = null
+    window.removeEventListener('resize', handleResize)
+    controls.dispose()
+  }
+}
+export default init
